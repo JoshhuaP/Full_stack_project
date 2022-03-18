39aab98606416ac7bd52b33091364a8b95cad576
Alexis Trupin <link9745@gmail.com>
2022-03-18 14:34:10 +0100

    adding categories controller with get routes

---
+++ api/src/main/java/fr/esiea/blogApi/controllers/CategoryController.java
@@ -0,0 +1,22 @@
+package fr.esiea.blogApi.controllers;
+
+import fr.esiea.blogApi.models.Category;
+import fr.esiea.blogApi.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping({ "", "/" })
+    public Iterable<Category> getCategories() {
+        return categoryService.getCategories();
+    }
+
+}
