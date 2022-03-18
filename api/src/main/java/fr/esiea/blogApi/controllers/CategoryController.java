e0737404336d4f33c24b8163421f1a7f53e82f8d
Alexis Trupin <link9745@gmail.com>
2022-03-18 14:37:29 +0100

    adding get by id in category controller

--- api/src/main/java/fr/esiea/blogApi/controllers/CategoryController.java
+++ api/src/main/java/fr/esiea/blogApi/controllers/CategoryController.java
@@ -2,8 +2,12 @@ package fr.esiea.blogApi.controllers;
 
 import fr.esiea.blogApi.models.Category;
 import fr.esiea.blogApi.services.CategoryService;
+import fr.esiea.blogApi.services.errors.NotFoundError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,16 @@ public class CategoryController {
     @GetMapping({ "", "/" })
     public Iterable<Category> getCategories() {
         return categoryService.getCategories();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Category> getCategory(@PathVariable("categoryId") final long id) {
+        try {
+            final Category category = categoryService.getCategory(id);
+            return new ResponseEntity<Category>(category, HttpStatus.OK);
+        } catch (final NotFoundError e) {
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
