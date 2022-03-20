349a2e78498c2c9ea5c78ac5f3be7c371c6182ff
Alexis Trupin <link9745@gmail.com>
2022-03-20 20:23:36 +0100

    fixing crossorigin

--- api/src/main/java/fr/esiea/blogApi/controllers/ArticleController.java
+++ api/src/main/java/fr/esiea/blogApi/controllers/ArticleController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/articles")
-@CrossOrigin(origins="*")
 public class ArticleController {
 
     @Autowired
--- api/src/main/java/fr/esiea/blogApi/controllers/RootController.java
+++ api/src/main/java/fr/esiea/blogApi/controllers/RootController.java
@@ -1,21 +1,10 @@
 package fr.esiea.blogApi.controllers;
 
-import fr.esiea.blogApi.services.ArticleService;
-import fr.esiea.blogApi.services.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="*")
 public class RootController {
-
-    @Autowired
-    private ArticleService articleService;
-
-    @Autowired
-    private CategoryService categoryService;
 
     @GetMapping("/")
     public String getRoot() {
