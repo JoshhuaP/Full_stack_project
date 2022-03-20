e14c3ec840a6d15fe71b02a4de6c097c55eeff38
Alexis Trupin <link9745@gmail.com>
2022-03-20 20:06:12 +0100

    adding crossorigin on all

--- api/src/main/java/fr/esiea/blogApi/controllers/CategoryController.java
+++ api/src/main/java/fr/esiea/blogApi/controllers/CategoryController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@CrossOrigin(origins="*")
 public class CategoryController {
 
     @Autowired
--- api/src/main/java/fr/esiea/blogApi/controllers/RootController.java
+++ api/src/main/java/fr/esiea/blogApi/controllers/RootController.java
@@ -3,10 +3,12 @@ package fr.esiea.blogApi.controllers;
 import fr.esiea.blogApi.services.ArticleService;
 import fr.esiea.blogApi.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*")
 public class RootController {
 
     @Autowired
